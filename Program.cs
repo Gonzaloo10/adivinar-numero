7d00aafabb1d08a8d7bbd7e2d4a10e3d17663fd5
Albertus06 <118900292+Albertus06@users.noreply.github.com>
2022-12-12 12:36:55 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Bienvenido jugador a Adivinar el número");
+Console.WriteLine("Te explico la reglas:");
+Console.WriteLine("1. Tienes que adivinar el número del 1 al 15");
+Console.WriteLine("2. Tienes que adivinar el número del 1 al 15");
+string nombre = Console.ReadLine();
+Console.WriteLine($"Tu nombre es {nombre}");
